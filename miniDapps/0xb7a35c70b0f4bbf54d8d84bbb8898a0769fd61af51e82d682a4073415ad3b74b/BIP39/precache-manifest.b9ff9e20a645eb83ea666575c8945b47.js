de3ddc208869e3b285b445f042c24f841516b9ed
drhuckle <glowkeeper@yahoo.co.uk>
2020-11-06 13:56:00 +0000

    miniDapps

---
+++ miniDapps/0xb7a35c70b0f4bbf54d8d84bbb8898a0769fd61af51e82d682a4073415ad3b74b/BIP39/precache-manifest.b9ff9e20a645eb83ea666575c8945b47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20247ccf0ec1e56bbfae43aa1ec78d9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b24f271b282eb670f34c",
+    "url": "./static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "245fb3171355b9dfde87",
+    "url": "./static/css/main.fea6712a.chunk.css"
+  },
+  {
+    "revision": "b24f271b282eb670f34c",
+    "url": "./static/js/2.81ebbb79.chunk.js"
+  },
+  {
+    "revision": "245fb3171355b9dfde87",
+    "url": "./static/js/main.930df4fb.chunk.js"
+  },
+  {
+    "revision": "5ddfd78ee0d473bb36b2",
+    "url": "./static/js/runtime-main.189fe623.js"
+  },
+  {
+    "revision": "aeddd528ad539cddd6542f855917ce96",
+    "url": "./static/media/coin-logo.aeddd528.svg"
+  }
+]);
