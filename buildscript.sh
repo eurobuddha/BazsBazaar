55ba329c03b8a0f45c33105f0ec623d75ff0f5d7
drhuckle <glowkeeper@yahoo.co.uk>
2021-01-17 12:58:20 +0000

    Version and description

---
+++ buildscript.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rm -rf ./bin
+
+mkdir bin
+
+zip -r ./bin/aMiniDarkRoom.minidapp aMiniDarkRoom
+
+zip -r ./bin/BIP39.minidapp BIP39
+
+zip -r ./bin/calculator.minidapp calculator
+
+zip -r ./bin/infiniteTetris.minidapp infiniteTetris
+
+zip -r ./bin/miniAlienInvasion.minidapp miniAlienInvasion
+
+zip -r ./bin/miniBrain.minidapp miniBrain
+
+zip -r ./bin/miniBreakLock.minidapp miniBreakLock
+
+zip -r ./bin/sciCalculator.minidapp sciCalculator
